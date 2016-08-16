6861d2fcd7ecfb625973fc1f723e780ec409fc78
Bing Pei <bpei@vmware.com>
2016-08-16 15:51:40 +0800

    Update my tools

--- myplatform/src/test/java/com/myplatform/web/test/TestBase.java
+++ myplatform/src/test/java/com/myplatform/web/test/TestBase.java
@@ -8,7 +8,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
-	"classpath:applicationContext.xml",
 	"classpath:test-applicationContext.xml"
 })
 /*
