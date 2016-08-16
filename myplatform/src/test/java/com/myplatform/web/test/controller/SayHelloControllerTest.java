36fea68de341ba45d79b5b434e929b51912c60d3
Bing Pei <bpei@vmware.com>
2016-08-16 18:36:43 +0800

    update config

--- myplatform/src/test/java/com/myplatform/web/test/controller/SayHelloControllerTest.java
+++ myplatform/src/test/java/com/myplatform/web/test/controller/SayHelloControllerTest.java
@@ -3,33 +3,43 @@ package com.myplatform.web.test.controller;
 import junit.framework.Assert;
 
 import com.myplatform.web.test.TestBase;
-
 import com.myplatform.web.controller.SayHelloController;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.ModelMap;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class SayHelloControllerTest extends TestBase {
 
-	@Autowired
+	@Mock
 	private SayHelloController sayHelloController;
 	
-	@Test
-	public void helloWorld() {
-		logger.debug("hello!");
-	}
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        sayHelloController = new SayHelloController();
+    }
+    
+    @After
+    public void tearDown() throws Exception {
+
+    }
 	
 	@Test
 	public void greetingTestFromContext() {
-		// given
 		String name = "Tom";
 		ModelMap model = new ModelMap();
 		
-		// when
-		String path = sayHelloController.greetPath(name, model);
+		String path = null;
+		when(sayHelloController.greetPath(name, model)).thenReturn(path);
 		
-		// then
 		Assert.assertEquals("greetings", path);
 		Assert.assertEquals(name, model.get("name"));
 	}
